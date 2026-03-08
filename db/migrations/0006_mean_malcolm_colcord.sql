c8d83571af082ac75f3744160b208de7ebf43aea
Michał Wilk <michalwilk139@gmail.com>
2026-03-09 00:37:28 +0100

    fix: migration fix

--- db/migrations/0006_mean_malcolm_colcord.sql
+++ db/migrations/0006_mean_malcolm_colcord.sql
@@ -1,4 +1,5 @@
 ALTER TABLE "file" ADD COLUMN "meeting_id" text;--> statement-breakpoint
+ALTER TABLE "folder" ADD COLUMN "meeting_id" text;--> statement-breakpoint
 ALTER TABLE "file" ADD CONSTRAINT "file_meeting_id_meeting_id_fk" FOREIGN KEY ("meeting_id") REFERENCES "public"."meeting"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "folder" ADD CONSTRAINT "folder_meeting_id_meeting_id_fk" FOREIGN KEY ("meeting_id") REFERENCES "public"."meeting"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 CREATE INDEX "file_meeting_idx" ON "file" USING btree ("meeting_id");--> statement-breakpoint
